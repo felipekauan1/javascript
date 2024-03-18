5db45f67f9100f7f0ad65e5219acb740eec3baf5
felipekauan1 <152967390+felipekauan1@users.noreply.github.com>
2024-03-18 05:39:34 -0300

    Create ambiente.js

---
+++ aula013/ambiente.js
@@ -0,0 +1,17 @@
+function contar1() {
+    var c = 1
+    do {
+        console.log(c)
+        c++
+    } while (c <= 10)  
+}
+
+function contar2() {
+    var c = 10
+    while (c >= 1) {
+        console.log(c)
+        c--
+    }
+}
+
+contar2()
